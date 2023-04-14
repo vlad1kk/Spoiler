1bcd7896a36eb75d93204ffa604bd6ce6dbd8530
Vlad Tipukhov <vlad1kk728@ukr.net>
2023-04-14 13:31:22 +0300

    add style and jQuery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    $('.header__burger').click(function(event){
-        $('.header__burger, .header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
+    $('.block__title').click(function(event){
+        $(this).toggleClass('active').next().slideToggle(300);
+    })
 });
